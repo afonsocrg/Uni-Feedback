974face31659822d51e1a7aa3c2615f43800d6de
afonsocrg <afonso.crg@gmail.com>
2025-07-25 19:56:32 +0700

    remove path alias for internal packages

--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -1,7 +1,14 @@
-// TODO: It would be cool if we could import from the package and not from a relative path
-// import '@uni-feedback/ui/style.css'
+// TODO: We need to fix this...
+// For some reason the local tailwind config is not
+// including the classes from the ui package (even though
+// we configured the `content` option in tailwind.config.js).
+// By importing both the src and dist styles, we can include
+// all the necessary styles, but this is a TERRIBLE workaround.
+
+// import '@uni-feedback/ui/style.css' // <-- Does not work
+// import '../../../packages/ui/dist/style.css' // <-- Does not work
 import '../../../packages/ui/dist/style.css'
-// import '../../../packages/ui/src/style.css'
+import '../../../packages/ui/src/style.css'
 import './index.css'
 
 import { StrictMode } from 'react'
