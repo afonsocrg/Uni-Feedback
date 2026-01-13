b6df55247659f88d2dba782477bf92877b881c09
afonsocrg <afonso.crg@gmail.com>
2026-01-13 18:54:30 +0000

    persist sidebar open state in localstorage (admin dashboard)

--- apps/dashboard/src/components/DashboardLayout.tsx
+++ apps/dashboard/src/components/DashboardLayout.tsx
@@ -1,11 +1,17 @@
 import { SidebarProvider, SidebarTrigger } from '@uni-feedback/ui'
+import { useLocalStorage } from '@uidotdev/usehooks'
 import { Outlet } from 'react-router-dom'
 import { DashboardSidebar } from './DashboardSidebar'
 import { Footer } from './Footer'
 
 export function DashboardLayout() {
+  const [sidebarOpen, setSidebarOpen] = useLocalStorage(
+    'dashboard_sidebar_open',
+    true
+  )
+
   return (
-    <SidebarProvider>
+    <SidebarProvider open={sidebarOpen} onOpenChange={setSidebarOpen}>
       <DashboardSidebar />
       <div className="w-full flex flex-col min-h-screen">
         <main className="p-4 flex-1">
