8a455cea87f629816ab03c668d5bc9ce53524082
afonsocrg <afonso.crg@gmail.com>
2026-05-31 17:07:11 +0200

    fix: use getLocalePath for profile skeleton detection in auth-layout

--- apps/website-ssr/app/routes/auth-layout.tsx
+++ apps/website-ssr/app/routes/auth-layout.tsx
@@ -1,14 +1,13 @@
 import { Outlet, useLocation } from 'react-router'
 import { AuthGuard, ProfilePageSkeleton } from '~/components'
+import { detectLang, getLocalePath } from '~/utils/i18n-routes'
 
 export default function AuthLayout() {
   const location = useLocation()
 
   const getLoadingComponent = () => {
-    if (
-      location.pathname === '/perfil' ||
-      location.pathname === '/en/profile'
-    ) {
+    const lang = detectLang(location.pathname)
+    if (location.pathname === getLocalePath('profile', lang)) {
       return <ProfilePageSkeleton />
     }
     return undefined
